7dd293cb59adeebd81dbefa492b0ba6530344468
Amaan Cheval <amaan.cheval@gmail.com>
2015-07-19 22:18:43 +0530

    Basic caching

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,42 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(...) {
+        set <- function(...) {
+                args <- c(as.list(environment()), list(...))
 
+                if (class(args[[1]]) == 'matrix') {
+                        internalMatrix <<- args[[1]]
+                }
+                else {
+                        internalMatrix <<- matrix(...)
+                }
+        }
+
+        get <- function() {
+                internalMatrix
+        }
+        
+        getInverse <- function() {
+                print('Getting inverse')
+                if (exists('inv')) {
+                        print('Inverse cached already')
+                        return(inv)
+                }
+                else {
+                        inv <<- solve(internalMatrix)
+                        return(inv)
+                }
+        }
+
+        set(...)
+        list(set = set, get = get, getInverse = getInverse, inv=inv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # Return a matrix that is the inverse of 'x'
+        x$getInverse()
 }
